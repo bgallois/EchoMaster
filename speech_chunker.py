9fd4dd4754e7e9222fb57ed1213927d05e3e7a62
bgallois <benjamin@gallois.cc>
2025-02-25 19:26:00 +0100

    [feat] add AI speech detector

--- speech_chunker.py
+++ speech_chunker.py
@@ -1,4 +1,5 @@
 import yt_dlp
+import torch
 import requests
 import pyaudio
 import os
@@ -7,6 +8,9 @@ from pydub import AudioSegment
 from pydub.silence import detect_nonsilent
 from pydub import AudioSegment
 import speech_recognition as sr
+import scipy
+import librosa
+from silero_vad import get_speech_timestamps, collect_chunks
 
 
 class SpeechChunker:
@@ -57,15 +61,35 @@ class SpeechChunker:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download(self._url)
 
-        self._data = AudioSegment.from_file("data.m4a", "m4a").normalize()
+        self._data = AudioSegment.silent(duration=1000) + AudioSegment.from_file(
+            "data.m4a", "m4a") + AudioSegment.silent(duration=1000)
 
-    def process(self, silence_thresh=-35, min_silence_len=500, seek_step=1):
-        # TODO: manual optimized detection
-        nonsilent_chunks = detect_nonsilent(
-            self._data,
-            min_silence_len=min_silence_len,
-            silence_thresh=silence_thresh,
-            seek_step=seek_step)
+    def process(self, silence_thresh=-30, min_silence_len=250, seek_step=1):
+        audio = np.frombuffer(self._data.raw_data, np.uint16)
+        audio = audio.reshape(len(audio) // 2, self._data.channels).sum(axis=1)
+        audio = librosa.util.normalize(audio.astype(np.float32))
+        audio = librosa.resample(
+            audio,
+            orig_sr=self._data.frame_rate,
+            target_sr=16000)
+        audio_tensor = torch.from_numpy(audio)
+        vad_model, utils = torch.hub.load(
+            'snakers4/silero-vad', model='silero_vad')
+        nonsilent_chunks = [
+            (i["start"] *
+             1000,
+             i["end"] *
+                1000) for i in get_speech_timestamps(
+                audio_tensor,
+                vad_model,
+                sampling_rate=16000,
+                threshold=0.2,
+                return_seconds=True,
+                speech_pad_ms=80,
+                min_speech_duration_ms=1000,
+                max_speech_duration_s=10,
+                min_silence_duration_ms=50)]
+        print(nonsilent_chunks)
         self._phrases = iter(nonsilent_chunks)
 
 
